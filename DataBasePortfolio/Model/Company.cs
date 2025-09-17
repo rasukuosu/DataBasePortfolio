790c6c2f9f4bdc4e20a04b7079e9831cba61db1a
rasukuosu <shirobakorx782@gmail.com>
2025-09-17 11:05:44 +0900

    プロジェクト ファイルを追加します。

---
+++ DataBasePortfolio/Model/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace DataBasePortfolio.Model
+{
+    [Table("Companys")]
+    class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }//DB用企業ID
+        [Required]
+        public string CompanyName { get; set; }//会社名    
+        [Required]
+        public string President  { get; set; }//代表取締役
+        [Required]
+        public string URL { get; set; }//会社概要のURL
+    }
+}
