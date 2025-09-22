d2939fac022046ba6520d080f1e3c8e7df5a098c
rasukuosu <shirobakorx782@gmail.com>
2025-09-22 11:00:14 +0900

    DbContextの作成

---
+++ DataBasePortfolio/Model/CompanyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBasePortfolio.Model
+{
+    class CompanyContext : DbContext
+    {
+        public DbSet<Company> Companys { get; set; } //DBの項目をマッピング
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //実際に使うDBの構築
+        {
+           optionsBuilder.UseSqlite("Data Source=company.db");
+           
+        }
+    }
+}
