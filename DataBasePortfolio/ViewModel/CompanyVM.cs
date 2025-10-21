dbe06e4ae38ef41e317ad0b7cd0fa9df0599ba83
rasukuosu <shirobakorx782@gmail.com>
2025-10-21 15:35:57 +0900

    ObservableCollectionの実装まで

--- DataBasePortfolio/ViewModel/CompanyVM.cs
+++ DataBasePortfolio/ViewModel/CompanyVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,23 @@ using DataBasePortfolio.Model;
 
 namespace DataBasePortfolio.ViewModel
 {
-    internal class CompanyVM: ObservableObject
+    internal partial class CompanyVM: ObservableObject
     {
-        private CompanyRepository _companyRepository;
+       
+        private CompanyRepository _companyRepository;//Modelのリポジトリを保持するフィールド
+        public ObservableCollection<Company> _companiesList;//Viewに表示するためのObservableCollection型のプロパティ
         CompanyVM(CompanyRepository repository)
         {
-             _companyRepository = repository;//Modelのリポジトリをインスタンス化
+             _companyRepository =  repository;//Modelのリポジトリをインスタンス化
+            var companyData = repository.GetAllCompanys();//一旦リポジトリからList型でデータを取得
+            _companiesList = new ObservableCollection<Company>(companyData);//ObservableCollectionにGetAllCompanyのリストを変換して格納
         }
        
         //Viewに表示するためのプロパティ
         [ObservableProperty]
         private Company _company;
+
+        
 
 
         //ここからWPF用のListViewの実装
