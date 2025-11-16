1b565df63eceee04f12afb75ca4de00455db29f3
rasukuosu <shirobakorx782@gmail.com>
2025-11-16 17:41:01 +0900

    seeding機能の完成CompaniesList = new ObservableCollection<Company>(companyData);にブレークPをおいて確認済み

--- DataBasePortfolio/Model/CompanyContext.cs
+++ DataBasePortfolio/Model/CompanyContext.cs
@@ -5,18 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using DataBasePortfolio.Migrations;
 
 namespace DataBasePortfolio.Model
 {
     class CompanyContext : DbContext
     {
         public DbSet<Company> Companys { get; set; } //DBの項目をマッピング
+        string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "company.db");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder //実際に使うDBの構築
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //実際に使うDBの構築
-        {
-            string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "company.db");
-            optionsBuilder.UseSqlite($"Data Source={dbPath}");
-           
+
+        .UseSqlite($"Data Source={dbPath}")
+        //DBの初期データ投入列がnullの時のみ、seedingを実行
+        .UseSeeding((context, _) =>
+            {
+            var Companys = context.Set<Company>().FirstOrDefault(c => c.CompanyName == "サンプルアニメ制作会社");
+            if (Companys == null) {
+                context.Set<Company>().Add(new Company { CompanyName = "サンプルアニメ制作会社", President = "サンプル描美" });
+                context.SaveChanges();
+            }
+        })
+        .UseAsyncSeeding(async(context, _, cancellationToken) =>
+            {
+            var Companys = await context.Set<Company>().FirstOrDefaultAsync(c => c.CompanyName == "サンプルアニメ制作会社", cancellationToken);
+                if (Companys == null)
+                {
+                    context.Set<Company>().AddAsync(new Company { CompanyName = "サンプルアニメ制作会社", President = "サンプル描美" });
+                    await context.SaveChangesAsync(cancellationToken);
+
+                }
+
+            });
         }
-    }
-}
+} 
+
