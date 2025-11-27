56cc80adb593d2791b74aba768e2bafe1bad8e28
rasukuosu <shirobakorx782@gmail.com>
2025-11-27 17:02:29 +0900

    CreateとRead完成

--- DataBasePortfolio/ViewModel/CompanyVM.cs
+++ DataBasePortfolio/ViewModel/CompanyVM.cs
@@ -14,7 +14,7 @@ namespace DataBasePortfolio.ViewModel
     {
 
         private CompanyRepository _companyRepository;//Modelのリポジトリを保持するフィールド
-        public ObservableCollection<Company> CompaniesList { get; }//Viewに表示するためのObservableCollection型のプロパティ
+        public ObservableCollection<Company> CompaniesList { get; set; }//Viewに表示するためのObservableCollection型のプロパティ
 
 
         public CompanyVM(CompanyRepository repository)
@@ -22,6 +22,7 @@ namespace DataBasePortfolio.ViewModel
             _companyRepository = repository;//Modelのリポジトリをインスタンス化
             var companyData = repository.GetAllCompanys();//一旦リポジトリからList型でデータを取得
             CompaniesList = new ObservableCollection<Company>(companyData);//ObservableCollectionにGetAllCompanyのリストを変換して格納
+            _Company = new Company();//Viewからの入力を受け取るためのプロパティを初期化
         }
         public CompanyVM() : this(new CompanyRepository())
         {
@@ -31,15 +32,26 @@ namespace DataBasePortfolio.ViewModel
         }
 
 
-        //Viewに表示するためのプロパティ
+        //Viewからの情報を保持するためのプロパティ
         [ObservableProperty]
         private Company _Company;
 
-     
+        [RelayCommand]
+        public void Create()///メソッド名 + "Command" = xaml側でbindしたプロパティ名
+        {
 
+            _companyRepository.AddCompany(_Company);//Modelのリポジトリを使ってDBに追加
+                CompaniesList.Add(_Company);//Viewに表示するためのObservableCollectionにも追加
+                 Company = new Company();//追加後、入力用のプロパティを初期化
 
-        //ここからWPF用のListViewの実装
-        //Viewに変更通知もできるようにrepositoryのqueryのToList（）したデータを
-        //ObservableCollectionにぶち込む
+        }
+        [RelayCommand]
+        public void Read(){
+
+            _companyRepository = new CompanyRepository();
+            var companyData = _companyRepository.GetAllCompanys();
+            CompaniesList = new ObservableCollection<Company>(companyData);
+
+        }
     }
 }
